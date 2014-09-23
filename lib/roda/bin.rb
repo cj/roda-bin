263abb82994b43b25838713766ae1530a232ea8d
cj <cjlazell@gmail.com>
2014-09-23 10:12:53 -0500

    no need for cli file

--- lib/roda/bin.rb
+++ lib/roda/bin.rb
@@ -2,5 +2,38 @@ require "roda/bin/version"
 
 class Roda
   module BIN
+    extend self
+
+    def server
+      args = [
+        'thin', 'start', '--threaded', '--max-persistent-conns', '300',
+        "--max-conns", "400", "-p", "8080"
+      ].concat(argv)
+
+      options = {
+        cmd: args.join(' '),
+        signal: 'TERM',
+        growl: true,
+        name: 'BrowserIO',
+        ignore: [],
+        dir: ["."],
+        pattern: "**/*.{rb,js,coffee,css,scss,sass,styl,erb,html,haml,ru,yml,slim,md}"
+      }
+
+      Rerun::Runner.keep_running(options[:cmd], options)
+    end
+
+    def envs
+      @envs ||= {}
+    end
+
+    private
+
+    def argv
+      @argv ||= begin
+        ARGV.shift
+        ARGV
+      end
+    end
   end
 end
